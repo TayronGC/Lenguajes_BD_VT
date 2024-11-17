9eb0152645fc37064cddd953165abd1afbcf5879
Mauma1306 <losmau1306@gmail.com>
2024-11-16 21:28:15 -0600

    Update index.php
    actulizacion de index

--- index.php
+++ index.php
@@ -9,3 +9,37 @@
     <h1>Pagina de Inicio</h1>
 </body>
 </html>
+
+
+
+<?php
+// Cargar los modelos y controladores automáticamente
+spl_autoload_register(function ($class) {
+    if (file_exists('controllers/' . $class . '.php')) {
+        require_once 'controllers/' . $class . '.php';
+    } elseif (file_exists('models/' . $class . '.php')) {
+        require_once 'models/' . $class . '.php';
+    }
+});
+
+// Conectar con la base de datos
+require_once 'models/Database.php';
+$database = new Database();
+$db = $database->getConnection();
+
+// Obtener el controlador y la acción desde la URL
+$controller = isset($_GET['controller']) ? ucfirst($_GET['controller']) . 'Controller' : 'UsuarioController';
+$action = isset($_GET['action']) ? $_GET['action'] : 'login';
+
+// Instanciar el controlador y ejecutar la acción
+if (class_exists($controller)) {
+    $controllerInstance = new $controller($db);
+    if (method_exists($controllerInstance, $action)) {
+        $controllerInstance->$action();
+    } else {
+        echo "La acción '$action' no existe en el controlador '$controller'.";
+    }
+} else {
+    echo "El controlador '$controller' no existe.";
+}
+?>
