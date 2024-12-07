774f628bb19a88b34fe7e382934058448f1eaef6
iespinoza421 <espinozamonteroisaac@gmail.com>
2024-12-06 19:16:12 -0600

    Front de Usuario

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,342 +1,119 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <!-- Basic -->
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <!-- Mobile Metas -->
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <!-- Site Metas -->
-    <meta name="keywords" content="" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-
-    <title>Dashboard</title>
-
-    <!-- slider stylesheet -->
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
-
-
-
-    <!-- bootstrap core css -->
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
-
-    <!-- fonts style -->
-    <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="../public/css/style.css" rel="stylesheet" />
-    <!-- responsive style -->
-    <link href="css/responsive.css" rel="stylesheet" />
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Macro Salud</title>
+    <link rel="stylesheet" href="/public/css/style.css">
 </head>
 
-
-
 <body>
-    <div class="hero_area">
-        <!-- header section strats -->
-        <header class="header_section">
-            <div class="container-fluid">
-                <nav class="navbar navbar-expand-lg custom_nav-container ">
-                    <a class="navbar-brand" href="index.php?controller=Dashboard&action=index">
-                        <span>
-                            Macro Salud
-                        </span>
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
-                            <ul class="navbar-nav">
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Productos populares
-                                    </a>
+    <header class="header">
 
-                                </li>
 
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="
-                                    false">CUIDADO PERSONAL</a>
-                                </li>
+        <div class="menu container">
 
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="
-                                    false">Inicio Sesión</a>
-                                </li>
-                            </ul>
-                            <div class="user_option">
-                                <form class="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
-                                    <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+            <a href="#" class="logo"><img src="/public/img/macrologo.png"></a>
+            <input type="checkbox" id="menu" />
+            <label for="menu">
+                <img src="/public/img/menu.svg" class="menu-icono" alt="">
+            </label>
 
-                </nav>
-                <hr style="border-color:white; margin-top: -10px;">
+            <nav class="navbar">
+                <ul>
+                    <li><a href="/views/login.php">Inicio de Sesion</a></li>
+                </ul>
+            </nav>
+            <div class="socials-1">
+                <a target="_blank" href="https://www.instagram.com/macrosalud/"><img src="/public/img/r1.svg" alt=""></a>
+                <a href="#form"><img src="/public/img/r2.svg" alt=""></a>
+                <a target="_blank" href="https://www.facebook.com/share/p/15DTKFsLLT/"><img src="/public/img/r3.svg" alt=""></a>
+            </div>
+        </div>
+
+        <div class="header-content container">
+            <div class="header-txt">
+                <h1>!Bienvenido a Macro Salud!</h1>
+                <p>"Explora nuestra amplia gama de productos y descubre cómo podemos ayudarte a alcanzar tus objetivos de salud. Desde suplementos alimenticios hasta productos de cuidado personal, tenemos todo lo que necesitas para sentirte mejor cada día."</p>
             </div>
 
+            <div class="header-img">
+                <img src="/public/img/sportmana-productos-1024x576.png" alt="">
+            </div>
 
-        </header>
-        <!-- end header section -->
-        <!-- slider section -->
-        <section class=" slider_section position-relative">
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <div class="container">
-                            <div class="col-lg-10 col-md-11 mx-auto">
-                                <div class="detail-box">
-                                    <div>
+        </div>
+    </header>
 
-                                        <h1>
-                                            Bienvenido a Macro Salud
-                                        </h1>
-                                        <p>
-                                            Accede fácilmente a todas las funcionalidades de nuestra tienda en un solo lugar.
-                                        </p>
-                                        <div class="">
-                                            <a onclick="window.location.href='#contacto'">
-                                                Contactenos
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+    <section class="about">
+        <div class="about-content container">
+            <img class="ab1" src="/public/img/ab1.png" alt="">
+            <img class="ab2" src="/public/img/ab1.png" alt="">
+            <img class="ab3" src="/public/img/ab1.png" alt="">
 
+            <div class="about-img">
+                <img src="/public/img/probioticos-capsulas-natural-systems.jpg" alt="">
+            </div>
+            <div class="about-txt">
+                <h2>Sobre Nosotros</h2>
+                <p>Ofrecemos una amplia variedad de productos naturales y saludables,
+                    cuidadosamente seleccionados para mejorar tu bienestar.
+                    Desde suplementos alimenticios hasta productos de cuidado personal,
+                    todos nuestros artículos están diseñados para ayudarte a llevar una vida más saludable
+                    y equilibrada.
+                </p>
+                <a href="#form" class="btn-2">Contáctenos</a>
+            </div>
+        </div>
+    </section>
 
-                </div>
-        </section>
-        <!-- end slider section -->
-    </div>
+    <section class="information">
 
+        <div class="information-content container">
+            <img class="in1" src="/public/img/ab1.png" alt="">
+            <img class="in2" src="/public/img/ab1.png" alt="">
+            <img class="in3" src="/public/img/ab1.png" alt="">
 
-    <!-- Us section -->
+            <h2>Informacion de los productos</h2>
+            <p>Nuestros productos están elaborados con ingredientes de la más alta calidad, garantizando su efectividad y seguridad. Cada producto ha sido sometido a rigurosos controles de calidad para asegurar que cumplan con los estándares más exigentes. Además, contamos con un equipo de expertos en salud y nutrición que están disponibles para brindarte asesoría personalizada
+                y ayudarte a elegir los productos que mejor se adapten a tus necesidades.
+            </p>
+        </div>
+    </section>
 
-    <section class="us_section layout_padding">
+    <main id="form" class="contact container">
+
+        <div class="form-content">
+
+            <h2>Contáctenos</h2>
+            <form action="https://formsubmit.co/macrosalud29@gmail.com" method="POST">
+                <input class="campo" type="text" name="name" placeholder="Nombre">
+                <input class="campo" type="email" name="email" placeholder="Correo Electronico">
+                <input class="btn-2" type="submit" value="Enviar">
+
+                <input type="hidden" name="_next" value="http://localhost:3000/views/dashboard.php">
+                <input type="hidden" name="_captcha" value="false">
+            </form>
+        </div>
+    </main>
+
+    <footer class="footer">
         <div class="container">
-            <div class="heading_container">
-                <h2>
-                    ¿Por qué elegirnos?
-                </h2>
-            </div>
-
-            <div class="us_container ">
-                <div class="row">
-                    <div class="col-lg-3 col-md-6">
-                        <div class="box">
-                            <div class="img-box">
-                                <img src="Images/u-1.png" alt="" width="60">
-                            </div>
-                            <div class="detail-box">
-                                <h5>
-                                    Los mejores productos.
-                                </h5>
-                                <p>
-                                    Manejamos una alta gama de productos de calidad para que puedas encontrar todo lo que necesitas en un solo lugar.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="box">
-                            <div class="img-box">
-                                <img src="Images/u-4.png" alt="" width="60">
-                            </div>
-                            <div class="detail-box">
-                                <h5>
-                                    Excelente servicio al cliente.
-                                </h5>
-                                <p>
-                                    Nuestro equipo de soporte está disponible para ayudarte en cualquier momento, ofreciendo asesoría personalizada para que puedas sacar el máximo provecho de nuestra plataforma.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="box">
-                            <div class="img-box">
-                                <img src="Images/u-2.png" alt="" width="60">
-                            </div>
-                            <div class="detail-box">
-                                <h5>
-                                    Pagina intuitiva.
-                                </h5>
-                                <p>
-                                    Nuestra plataforma está diseñada para que puedas navegar de forma sencilla y rápida, encontrando todo lo que necesitas en un solo lugar.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="box">
-                            <div class="img-box">
-                                <img src="Images/u-3.png" alt="" width="60">
-                            </div>
-                            <div class="detail-box">
-                                <h5>
-                                    Soporte al Cliente Personalizado
-                                </h5>
-                                <p>
-                                    Nuestro equipo de soporte está disponible para ayudarte en cualquier momento, ofreciendo asesoría personalizada para que puedas sacar el máximo provecho de nuestra plataforma.
-                                </p>
-                            </div>
-                        </div>
+            <div class="footer-links">
+                <div class="link-img">
+                    <img src="/public/img/macrologo.png" alt="">
+                    <div class="socials-2">
+                        <a target="_blank" href="https://www.instagram.com/macrosalud/"><img src="/public/img/r1.svg" alt=""></a>
+                        <a href="#form"><img src="/public/img/r2.svg" alt=""></a>
+                        <a target="_blank" href="https://www.facebook.com/share/p/15DTKFsLLT/"><img src="/public/img/r3.svg" alt=""></a>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-
-    <!-- end us section -->
-
-
-    <!-- heathy section -->
-
-    <section class="heathy_section layout_padding">
-        <div class="container">
-
-            <div class="row">
-                <div class="col-md-12 mx-auto">
-                    <div class="detail-box">
-                        <h2>
-                            Nuestra Mision
-                        </h2>
-                        <p>
-                            En Macro Salud, Ofrecemos una amplia variedad de productos de alta calidad y un servicio excepcional que está diseñado para satisfacer todas tus necesidades diarias y mejorar tu experiencia de compra. </p>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </section>
-
-    <!-- end heathy section -->
-
-    <!-- trainer section -->
-
-
-
-    <!-- end trainer section -->
-
-    <!-- contact section -->
-
-    <section class="contact_section " id="contacto">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-6 px-0">
-                    <div class="img-box">
-                        <img src="" alt="">
-                    </div>
-                </div>
-                <div class="col-lg-5 col-md-6">
-                    <div class="form_container pr-0 pr-lg-5 mr-0 mr-lg-2">
-                        <div class="heading_container">
-                            <h2>
-                                Contactenos
-                            </h2>
-                        </div>
-                        <form action="https://formsubmit.co/inteligenteahorra@gmail.com" method="POST">
-
-                            <div>
-                                <label for="name">Nombre</label>
-                                <input type="text" name="name" id="name" placeholder="Nombre" required="true" />
-                            </div>
-                            <div>
-                                <label for="email">Correo Electrónico</label>
-                                <input type="email" name="email" id="email" placeholder="Correo Electrónico" required="true" />
-                            </div>
-                            <div>
-                                <label for="subject">Asunto</label>
-                                <input type="text" name="subject" id="subject" placeholder="Asunto" required="true" />
-                            </div>
-                            <div>
-                                <label for="comments">Mensaje</label>
-                                <input type="text" class="message-box" name="comments" id="comments" placeholder="Mensaje" required="true" />
-                            </div>
-
-                            <div class="d-flex ">
-                                <input class="btn" type="submit" value="enviar">
-                                <input type="hidden" value="http://localhost:3000/index.php?controller=Dashboard&action=index" name="_next">
-                                <input type="hidden" value="false" name="_captcha">
-                            </div>
-                        </form>
-                    </div>
-                </div>
+            <div class="footer-copy">
+                <p>Lenguajes de Bases de Datos. Grupo 2. Todos los derechos reservados.</p>
             </div>
         </div>
-    </section>
-
-    <!-- end contact section -->
-
-    <!-- info section -->
-    <section class="info_section layout_padding2">
-        <div class="container">
-            <div class="info_items">
-                <a href="">
-                    <div class="item ">
-                        <div class="img-box box-1">
-                            <img src="" alt="">
-                        </div>
-                        <div class="detail-box">
-                            <p>
-                                Costa Rica
-                            </p>
-                        </div>
-                    </div>
-                </a>
-                <a href="">
-                    <div class="item ">
-                        <div class="img-box box-2">
-                            <img src="" alt="">
-                        </div>
-                        <div class="detail-box">
-                            <p>
-                                +506 2020-4040
-                            </p>
-                        </div>
-                    </div>
-                </a>
-                <a href="">
-                    <div class="item ">
-                        <div class="img-box box-3">
-                            <img src="" alt="">
-                        </div>
-                        <div class="detail-box">
-                            <p>
-                                MacroSalud@gmail.com
-                            </p>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </div>
-    </section>
-
-    <!-- end info_section -->
-
-    <!-- footer section -->
-    <footer class="container-fluid footer_section">
-        <p>
-            &copy; 2024 All Rights Reserved. Design by
-            <a>Grupo 2-Fidelitas (ISAAC ESPINOZA, TAYRON GUZMAN, BUNTARO OLIVARES, MAURICIO MARIN)</a>
-        </p>
     </footer>
-    <!-- footer section -->
-
-    <script src="js/jquery-3.4.1.min.js"></script>
-    <script src="js/bootstrap.js"></script>
-
 </body>
-
 </html>
